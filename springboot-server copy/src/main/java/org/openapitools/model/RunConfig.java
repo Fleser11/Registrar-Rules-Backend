d9f787ef0311732be254e86056a3dbb8e0a8883b
Fleser, Connor <cfleser11@gmail.com>
2025-06-29 22:33:41 -0500

    backend for the registrar tool

---
+++ springboot-server copy/src/main/java/org/openapitools/model/RunConfig.java
@@ -0,0 +1,144 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.openapitools.model.CourseConfigInner;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * RunConfig
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-06-29T19:51:38.547232257-05:00[America/Chicago]", comments = "Generator version: 7.6.0")
+public class RunConfig {
+
+  private String degreeProgram;
+
+  private String genEdProgram;
+
+  @Valid
+  private List<@Valid CourseConfigInner> courseConfig = new ArrayList<>();
+
+  public RunConfig degreeProgram(String degreeProgram) {
+    this.degreeProgram = degreeProgram;
+    return this;
+  }
+
+  /**
+   * Get degreeProgram
+   * @return degreeProgram
+  */
+  
+  @Schema(name = "degreeProgram", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("degreeProgram")
+  public String getDegreeProgram() {
+    return degreeProgram;
+  }
+
+  public void setDegreeProgram(String degreeProgram) {
+    this.degreeProgram = degreeProgram;
+  }
+
+  public RunConfig genEdProgram(String genEdProgram) {
+    this.genEdProgram = genEdProgram;
+    return this;
+  }
+
+  /**
+   * Get genEdProgram
+   * @return genEdProgram
+  */
+  
+  @Schema(name = "genEdProgram", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("genEdProgram")
+  public String getGenEdProgram() {
+    return genEdProgram;
+  }
+
+  public void setGenEdProgram(String genEdProgram) {
+    this.genEdProgram = genEdProgram;
+  }
+
+  public RunConfig courseConfig(List<@Valid CourseConfigInner> courseConfig) {
+    this.courseConfig = courseConfig;
+    return this;
+  }
+
+  public RunConfig addCourseConfigItem(CourseConfigInner courseConfigItem) {
+    if (this.courseConfig == null) {
+      this.courseConfig = new ArrayList<>();
+    }
+    this.courseConfig.add(courseConfigItem);
+    return this;
+  }
+
+  /**
+   * Get courseConfig
+   * @return courseConfig
+  */
+  @Valid 
+  @Schema(name = "courseConfig", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("courseConfig")
+  public List<@Valid CourseConfigInner> getCourseConfig() {
+    return courseConfig;
+  }
+
+  public void setCourseConfig(List<@Valid CourseConfigInner> courseConfig) {
+    this.courseConfig = courseConfig;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    RunConfig runConfig = (RunConfig) o;
+    return Objects.equals(this.degreeProgram, runConfig.degreeProgram) &&
+        Objects.equals(this.genEdProgram, runConfig.genEdProgram) &&
+        Objects.equals(this.courseConfig, runConfig.courseConfig);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(degreeProgram, genEdProgram, courseConfig);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class RunConfig {\n");
+    sb.append("    degreeProgram: ").append(toIndentedString(degreeProgram)).append("\n");
+    sb.append("    genEdProgram: ").append(toIndentedString(genEdProgram)).append("\n");
+    sb.append("    courseConfig: ").append(toIndentedString(courseConfig)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ temp-gen/src/main/java/org/openapitools/model/SemConfig.java
@@ -0,0 +1,119 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * SemConfig
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-06-29T22:01:34.789336166-05:00[America/Chicago]", comments = "Generator version: 7.6.0")
+public class SemConfig {
+
+  private Integer numCourses;
+
+  @Valid
+  private List<String> courses = new ArrayList<>();
+
+  public SemConfig numCourses(Integer numCourses) {
+    this.numCourses = numCourses;
+    return this;
+  }
+
+  /**
+   * Get numCourses
+   * @return numCourses
+  */
+  
+  @Schema(name = "numCourses", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("numCourses")
+  public Integer getNumCourses() {
+    return numCourses;
+  }
+
+  public void setNumCourses(Integer numCourses) {
+    this.numCourses = numCourses;
+  }
+
+  public SemConfig courses(List<String> courses) {
+    this.courses = courses;
+    return this;
+  }
+
+  public SemConfig addCoursesItem(String coursesItem) {
+    if (this.courses == null) {
+      this.courses = new ArrayList<>();
+    }
+    this.courses.add(coursesItem);
+    return this;
+  }
+
+  /**
+   * Get courses
+   * @return courses
+  */
+  
+  @Schema(name = "courses", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("courses")
+  public List<String> getCourses() {
+    return courses;
+  }
+
+  public void setCourses(List<String> courses) {
+    this.courses = courses;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SemConfig semConfig = (SemConfig) o;
+    return Objects.equals(this.numCourses, semConfig.numCourses) &&
+        Objects.equals(this.courses, semConfig.courses);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(numCourses, courses);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class SemConfig {\n");
+    sb.append("    numCourses: ").append(toIndentedString(numCourses)).append("\n");
+    sb.append("    courses: ").append(toIndentedString(courses)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
